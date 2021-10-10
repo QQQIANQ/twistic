fa0f0d1e93a47c25d482e71623c9013f73ac94ff
maqian <maqian@hotmail.fr>
2021-10-10 23:26:33 +0200

    signUp page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 void main() async {
-  final FirebaseAuth auth = FirebaseAuth.instance;
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
